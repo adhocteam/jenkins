47030a8dbcd92b34c3ace118e68dbbc04ec550da
Robert Fairhead <bob@adhocteam.us>
2019-01-31 21:21:34 -0600

    Switch to va-demo specific entries

---
+++ jobs/setup_repo.groovy
@@ -0,0 +1,12 @@
+multibranchPipelineJob("va-demo") {
+    displayName "VA Demo"
+    description "https://github.com/adhocteam/va-demo"
+
+    branchSources {
+        github {
+            scanCredentialsId('github-user')
+            repoOwner('adhocteam')
+            repository('sample-task-toolkit')
+        }
+    }
+}
