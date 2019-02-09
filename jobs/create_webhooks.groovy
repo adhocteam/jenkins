c5eb3491f16b7f8b5b2c3a10ece953625c437b72
Robert Fairhead <bob@adhocteam.us>
2019-02-08 19:44:26 -0600

    Work on seed job

---
+++ jobs/create_webhooks.groovy
@@ -0,0 +1,19 @@
+jobs = Jenkins.instance.getAllItems()
+jobs.each { j ->
+
+    if (j instanceof jenkins.branch.MultiBranchProject) {
+        println('j.fullName: ' + j.fullName)
+      	for (scm in j.getSCMSources()) {
+            println('scm: ' + scm.getRepository())
+            scm.afterSave()
+        }
+    }
+
+    if (j instanceof jenkins.branch.OrganizationFolder) {
+        println('j.fullName: ' + j.fullName)
+        for (navigator in j.navigators) {
+            println('navigator: ' + navigator)
+            navigator.afterSave(j)
+        }
+    }
+}
--- jobs/setup_repo.groovy
+++ jobs/setup_repo.groovy
@@ -9,7 +9,25 @@ multibranchPipelineJob("va-demo") {
             repository('sample-task-toolkit')
         }
     }
-    triggers {
-        githubPush()
+
+    // As a back-up look for new branches/PRs once per day if no pushes
+    configure { node ->
+        node / triggers / 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+        spec('H H * * *')
+        interval(86400000)
+        }
+    }
+
+    // Find all branches and any PRs in the "merged with target" state
+    configure {
+        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
+
+        traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+            strategyId(3)
+        }
+        traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+            strategyId(1)
+        }
     }
 }
