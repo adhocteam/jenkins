a3237f2f9dfb29686b82547e95a791d8fabd4241
Robert Fairhead <bob@adhocteam.us>
2019-03-20 14:30:57 -0500

    Add TagDiscovery to our default Jenkins build

--- jobs/setup_gh_org_folder.groovy
+++ jobs/setup_gh_org_folder.groovy
@@ -23,6 +23,7 @@ organizationFolder('Adhocteam Github') {
         traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
             strategyId 1
         }
+        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait'
     }
 
     // "Project Recognizers"
