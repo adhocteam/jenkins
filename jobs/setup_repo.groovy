934388be7e1710c186e829f884224f719ac80a91
Robert Fairhead <robertfairhead@users.noreply.github.com>
2019-05-29 21:47:00 -0500

    Bob/single repo tags (#16)
    * Add tag discovery to default single repo seed job
    
    * Update comment

--- jobs/setup_repo.groovy
+++ jobs/setup_repo.groovy
@@ -1,20 +1,12 @@
-multibranchPipelineJob("va-demo") {
-    displayName "VA Demo"
-    description "https://github.com/adhocteam/va-demo"
+multibranchPipelineJob("demo") {
+    displayName "Demo"
+    description "https://github.com/adhocteam/"
 
     branchSources {
         github {
             scanCredentialsId('github-user')
             repoOwner('adhocteam')
             repository('sample-task-toolkit')
-        }
-    }
-
-    // As a back-up look for new branches/PRs once per day if no pushes
-    configure { node ->
-        node / triggers / 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
-        spec('H H * * *')
-        interval(86400000)
         }
     }
 
@@ -29,5 +21,26 @@ multibranchPipelineJob("va-demo") {
         traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
             strategyId(1)
         }
+        traits << 'org.jenkinsci.plugins.github__branch__source.TagDiscoveryTrait' {}
+
+        // Build strategies to enable tag discovery
+        def buildStrategies = it / buildStrategies
+        buildStrategies << 'jenkins.branch.buildstrategies.basic.BranchBuildStrategyImpl' {}
+        buildStrategies << 'jenkins.branch.buildstrategies.basic.ChangeRequestBuildStrategyImpl' {
+            ignoreTargetOnlyChanges false
+        }
+        buildStrategies << 'jenkins.branch.buildstrategies.basic.TagBuildStrategyImpl' {
+            atLeastMillis '-1'
+            atMostMillis '86400000'
+        }
     }
+
+    // As a back-up look for new branches/PRs once per day if no pushes
+    configure { node ->
+        node / triggers / 'com.cloudbees.hudson.plugins.folder.computed.PeriodicFolderTrigger' {
+        spec('H H * * *')
+        interval(86400000)
+        }
+    }
+
 }
